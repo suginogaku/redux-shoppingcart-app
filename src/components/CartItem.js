c33bd69abd271281c6f4bb2da87df22f36240c94
gakusugino <g-sugino@macro-eg.co.jp>
2025-05-29 17:05:28 +0900

    Storeの中身から商品情報を取り出して出力

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import { MinusIcon, PlusIcon } from '../HeroIcons';
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+  const { title, img, price, amount } = item;
+
   return (
-    <div>CartItem</div>
+    <article className='cart-item'>
+      <img src={img} alt='' />
+      <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>{price}円</h4>
+        <button className='remove-btn'>削除</button>
+      </div>
+      <div>
+        <button className='amount-btn'>
+          <PlusIcon />
+        </button>
+        <p className='amount'>{amount}</p>
+        <button className='amount-btn'>
+          <MinusIcon />
+        </button>
+      </div>
+    </article>
   )
 }
 

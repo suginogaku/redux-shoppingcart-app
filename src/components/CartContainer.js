01fe50d17d808955933c19a3555796d57d57b64d
gakusugino <g-sugino@macro-eg.co.jp>
2025-05-29 16:49:31 +0900

    買い物かごの中身を表示するコンポーネント作成

---
+++ src/components/CartContainer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem'
+
+const CartContainer = () => {
+  const { amount, cartItems, total } = useSelector((slice) => slice.cart)
+  if (amount < 1) {
+    return (
+      <section className='cart'>
+        <header>
+          <h2>買い物かご</h2>
+          <h4 className='empty-cart'>なにも入っていません</h4>
+        </header>
+      </section>
+    )
+  }
+
+  return (
+    <section className='cart'>
+      <header>
+        <h2>買い物かご</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} item={item} />
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className='cart-total'>
+          <h4>
+            合計 <span>{total}円</span>
+          </h4>
+        </div>
+        <button className='btn clear-btn'>全削除</button>
+      </footer>
+    </section>
+  )
+}
+
+export default CartContainer

088c3125b91fe83daf766d766c18d8bc421cc623
gakusugino <g-sugino@macro-eg.co.jp>
2025-05-30 14:57:19 +0900

    カートの状態が変化するたびに発火するようuseEffectで制御

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
 
 function App() {
+  // cartのstateが変化するたびにcalculateTotalsをdispatchするuseEffect
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cart]);
+
   return (
     <main>
       <Navbar />
